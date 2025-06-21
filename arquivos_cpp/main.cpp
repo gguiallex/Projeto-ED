0c1cea5fd11681cacdcd1fd59cb6191b65f74e28
Daniel <danieljesus7k@gmail.com>
2025-06-21 14:34:06 -0300

    Alteração do menu e da main para funcionamento correto

--- arquivos_cpp/main.cpp
+++ arquivos_cpp/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "manipuladorBinario.h"
+
+
 using namespace std;
+
+
 
 void Menu()
 {
     cout << "\n=====================================\n";
     cout << "        MENU DE OPÇÕES\n";
     cout << "=====================================\n";
-    cout << " 1 - Adicionar Esporte\n";
+    cout << " 1 - Inserir Registro\n";
     cout << " 2 - Visualizar Entre\n";
     cout << " 3 - Alterar Registro\n";
     cout << " 4 - Trocar Posição\n";
@@ -20,8 +24,9 @@ void Menu()
 
 int main()
 {
-    manipuladorBinario m("dados.bin");
-    m.converterCsvParaBinario("data_athlete_event.csv", "dados.bin");
+    manipuladorBinario m("dados1.bin");
+    m.converterCsvParaBinario("data_athlete_event.csv", "dados1.bin");
+    m.ordenarMergeMultiway();
 
     int numero;
     do
@@ -46,17 +51,27 @@ int main()
         }
         case 3:
         {
-            // m.alterarRegistroNaPosicao();
+            int pos;
+            cout << "Informe a posição do registro que deseja alterar: ";
+            cin >> pos;
+            Registro novo;
+            novo.lerTeclado(); 
+            m.alterarRegistroNaPosicao(pos, novo);
             break;
         }
         case 4:
         {
-            // m.trocarRegistros();
+            int pos1, pos2;
+            cout << "Informe a primeira posição: ";
+            cin >> pos1;
+            cout << "Informe a segunda posição: ";
+            cin >> pos2;
+            m.trocarRegistros(pos1, pos2);
             break;
         }
         case 5:
         {
-            // m.ordenarMergeMultiway("ordenado.bin");
+            m.ordenarMergeMultiway();
             break;
         }
         case 6:
